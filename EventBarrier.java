4772e85e3cac486872429724e2871453b9efccf7
Ryan Fishel <fishel.ryan@gmail.com>
2014-03-17 20:48:57 -0400

    created EventBarrier Class

---
+++ EventBarrier.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @author Ryan Fishel
+ * @author Ryan Toussaint
+ * @author Lyndsay Kerwin
+ * @author Carlos Reyes
+ *
+ */
+
+public class EventBarrier extends AbstractEventBarrier{
+
+	@Override
+	public void arrive() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void raise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void complete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int waiters() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

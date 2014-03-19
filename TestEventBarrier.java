56f441eb80cfe21d17d3b018263afac3414fc3ea
Ryan Fishel <fishel.ryan@gmail.com>
2014-03-18 21:08:30 -0400

    fixed a assert statement

--- TestEventBarrier.java
+++ TestEventBarrier.java
@@ -10,6 +10,8 @@ public class TestEventBarrier {
 	private Minstrels m2 = new Minstrels(bar);
 	private Minstrels m3 = new Minstrels(bar);
 	private Minstrels m4 = new Minstrels(bar);
+	private Minstrels m5 = new Minstrels(bar);
+	private Minstrels m6 = new Minstrels(bar);
 	private GateKeeper gk = new GateKeeper(bar);
 	
 	@Test
@@ -20,15 +22,20 @@ public class TestEventBarrier {
 		assertFalse(bar.getEventOccuring());
 		gk.start();
 
+		//m4 we want to get through while the gate is up
 		m4.start();
 
-		Thread.sleep(100);
+		//Thread.sleep(100);
+		//we want these to stop at the gate because it has been lowered
+		m5.start();
+		m6.start();
 
 		assertTrue(m1.getIntoCastle());
 		assertTrue(m2.getIntoCastle());
 		assertTrue(m3.getIntoCastle());
 		assertTrue(m4.getIntoCastle());
-		
+		assertFalse(m5.getIntoCastle());
+		assertFalse(m6.getIntoCastle());
 		assertFalse(bar.getEventOccuring());
 		
 	}

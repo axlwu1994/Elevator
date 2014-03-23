9103e7004547cedba0d934117e2ff6e757453d22
CarlosReyesStoneham <cer26@duke.edu>
2014-03-23 17:25:26 -0400

    added to parser

--- Parser.java
+++ Parser.java
@@ -4,19 +4,30 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
+	
+	int myNumFloors;
+	int myNumElevators;
+	
+	
 	public void readFile(File input) {
 		List<Rider> riderList = new ArrayList<Rider>();
 		
 		Scanner in = new Scanner(System.in);
 		int i = in.nextInt();
 		
+		//This gets the building numfloors and numelevators
+		String firstLine = in.nextLine();
+		String[] aboutBuilding = firstLine.split(" ");
+		int numFloors = Integer.parseInt(aboutBuilding[0]);
+		int numElevators = Integer.parseInt(aboutBuilding[1]);
+		
+		//This gets all the riders
 		while (in.hasNext()) {
 			String line = in.nextLine();
 			String[] floors = line.split(" ");
 			int start = Integer.parseInt(floors[0]);
 			int destination = Integer.parseInt(floors[1]);
 			
-			//DONT ACTUALLY ADD THIS
 			Building building = new Building(1, 1);
 			
 			Rider rider = new Rider(building, start, destination);
